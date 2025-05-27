acd37d744ea6295ddbc6436cf9077dd7abc12a67
teuku <teukusalmanfarizi2003@gmail.com>
2025-05-27 16:50:45 +0700

    Fixing PWA and Notif

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/story-app/',
+  base: '/stroy-app/',
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'src', 'public'),
   build: {
@@ -18,20 +18,35 @@ export default defineConfig({
 
   plugins: [
     VitePWA({
-  registerType: 'autoUpdate',
-  manifest: {
-    name: 'Story App',
-    short_name: 'Story App',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#42b883',
-  },
-  workbox: {
-    globPatterns: ['**/*.{js,css,html}'],
-  },
-  srcDir: 'public',
-  filename: 'sw.js',
-}),
+      base: '/stroy-app/',
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Story App',
+        short_name: 'Story App',
+        description: 'A simple application to share stories.',
+        start_url: '/stroy-app/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#42b883',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html}'],
+      },
+      srcDir: 'public',
+      filename: 'sw.js',
+    }),
   ],
 });
